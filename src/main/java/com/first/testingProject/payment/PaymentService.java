4a294f5f72ae6604da5647cea382bb2379ced6d5
Ankita Srivastava <ankita.sr009@gmail.com>
2024-09-15 18:38:19 +0100

    PaymentFeatures

--- src/main/java/com/first/testingProject/payment/PaymentService.java
+++ src/main/java/com/first/testingProject/payment/PaymentService.java
@@ -4,10 +4,13 @@ import com.first.testingProject.customer.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class PaymentService {
+
+    private static final List<Currency> ACCEPTED_CURRENCIES = List.of(Currency.USD, Currency.GBP);
 
     private final CustomerRepository customerRepository;
     private final PaymentRepository paymentRepository ;
@@ -25,6 +28,48 @@ public class PaymentService {
     }
 
     void chargeCard(UUID customerId, PaymentRequest paymentRequest){
+        //1. Does customer exists if not throw
+        boolean isCustomerFound = customerRepository.findById(customerId).isPresent() ;
+
+        if(!isCustomerFound){
+            throw new IllegalStateException( String.format("Customer with id [%s] not found", customerId));
+
+        }
+
+        //2. Do we support the currency if not throw
+        boolean isCurrencySupported = ACCEPTED_CURRENCIES.stream()
+                .anyMatch( c -> c.equals(paymentRequest.getPayment().getCurrency()));
+
+        if(!isCurrencySupported){
+            String message = String.format(
+                    "Currency[%s] not supported",
+                    paymentRequest.getPayment().getCurrency());
+            throw new IllegalStateException(message);
+
+        }
+
+        //3. Charge card
+        CardPaymentCharge cardPaymentCharge = cardPaymentCharger.chargeCard(
+                paymentRequest.getPayment().getSource(),
+                paymentRequest.getPayment().getAmount(),
+                paymentRequest.getPayment().getCurrency(),
+                paymentRequest.getPayment().getDescription()
+        );
+
+        // 4. If not debited throw
+        if( !cardPaymentCharge.isCardDebited()){
+            throw  new IllegalStateException(String.format("Card not debited for customer %s", customerId));
+
+        }
+
+        // 5. Insert payment
+        paymentRequest.getPayment().setCustomerId(customerId);
+
+        paymentRepository.save(paymentRequest.getPayment());
+
+        //6. TO DO: send sms
+
+
 
     }
 }
