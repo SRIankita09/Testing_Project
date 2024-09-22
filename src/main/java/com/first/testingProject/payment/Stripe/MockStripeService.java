b6e7c64edae1b393d918782ddfbdc4b4ed2d6429
Ankita Srivastava <ankita.sr009@gmail.com>
2024-09-22 12:56:24 +0100

    Integration_testing

---
+++ src/main/java/com/first/testingProject/payment/Stripe/MockStripeService.java
@@ -0,0 +1,23 @@
+package com.first.testingProject.payment.Stripe;
+import com.first.testingProject.payment.CardPaymentCharge;
+import com.first.testingProject.payment.CardPaymentCharger;
+import com.first.testingProject.payment.Currency;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@ConditionalOnProperty(
+        value = "stripe.enabled",
+        havingValue = "false"
+)
+public class MockStripeService implements CardPaymentCharger {
+    @Override
+    public CardPaymentCharge chargeCard(String cardSource,
+                                        BigDecimal amount,
+                                        Currency currency,
+                                        String description) {
+        return new CardPaymentCharge(true);
+    }
+}
